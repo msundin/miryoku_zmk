802d26701cffe8e5e4ee3a8cfffb618e90f24b39
msundin <msundin.git@gmail.com>
2024-08-06 01:56:56 +0200

    snap

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,5 +5,5 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
-#define HRML(MOD, TAP) &u_hrml MOD TAP
-#define HRMR(MOD, TAP) &u_hrmr MOD TAP
+#define U_HRML(MOD, TAP) &u_hrml MOD TAP
+#define U_HRMR(MOD, TAP) &u_hrmr MOD TAP
