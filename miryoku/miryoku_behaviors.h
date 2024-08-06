4852f1342f38b329ac64c9d9f9c2c37333f22c50
msundin <msundin.git@gmail.com>
2024-08-06 02:45:47 +0200

    refactor for more robust customization structurs - compiling

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -6,5 +6,6 @@
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
 
+// Custom behaviors
 #define U_HRML(MOD, TAP) &u_hrml MOD TAP
 #define U_HRMR(MOD, TAP) &u_hrmr MOD TAP
