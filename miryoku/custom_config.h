3cc687793022b7e5aa741e9dc38a76c28e559c6d
msundin <msundin.git@gmail.com>
2024-08-09 22:19:47 +0200

    fix ö

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -21,7 +21,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_LAYER_MEDIA \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,\
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_RGB_TOG,         &kp RA(W),         &kp RA(Q),         &kp(P),            U_RGB_BRI,\
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_RGB_TOG,         &kp RA(W),         &kp RA(Q),         &kp RA(P),         U_RGB_BRI,\
 U_NA,              &kp RALT,          &u_to_U_FUN,       &u_to_U_MEDIA,     U_NA,              &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        U_EP_TOG,\
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
 
